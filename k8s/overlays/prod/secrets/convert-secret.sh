e40fa5789a58574c9e55ebce6e6cd3e01e82709b
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2025-01-19 21:31:18 +0200

    ci: update secrets with argocd order

---
+++ k8s/overlays/prod/secrets/convert-secret.sh
@@ -0,0 +1,10 @@
+for secret_file in *.secret.yaml; do
+    sealed_file="sealed-${secret_file/.secret.yaml/.yaml}"
+    kubeseal --format=yaml \
+        --controller-namespace=kube-system \
+        --controller-name=sealed-secrets-controller \
+        --scope namespace-wide \
+        --namespace=musclecode \
+        < "$secret_file" | \
+        yq e '.metadata.annotations += {"argocd.argoproj.io/hook": "PreSync", "argocd.argoproj.io/sync-wave": "-5"}' - > "$sealed_file"
+done
