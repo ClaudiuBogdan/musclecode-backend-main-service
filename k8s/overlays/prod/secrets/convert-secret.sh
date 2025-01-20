965a14cf39611b0a8f0ca5fe5c46ac0e68538976
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2025-01-20 12:50:16 +0200

    ci: update secrets

--- k8s/overlays/prod/secrets/convert-secret.sh
+++ k8s/overlays/prod/secrets/convert-secret.sh
@@ -3,7 +3,7 @@ for secret_file in *.secret.yaml; do
     kubeseal --format=yaml \
         --controller-namespace=kube-system \
         --controller-name=sealed-secrets-controller \
-        --scope namespace-wide \
+        --scope strict \
         --namespace=musclecode-prod \
         < "$secret_file" | \
         yq e '.metadata.annotations += {"argocd.argoproj.io/sync-wave": "-5"}' - > "$sealed_file"
